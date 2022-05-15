5d225f05afa920aec59f16e00a2263403242af59
fahd <trey34809@gmail.com>
2022-05-15 20:30:18 +0100

    khdmo meana asahbi

--- minishell.c
+++ minishell.c
@@ -6,27 +6,26 @@
 /*   By: fahd <fahd@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 18:45:32 by fahd              #+#    #+#             */
-/*   Updated: 2022/05/15 19:34:20 by fahd             ###   ########.fr       */
+/*   Updated: 2022/05/15 20:29:52 by fahd             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include <readline/readline.h>
-#include <readline/history.h>
-
 
 int main()
 {
     char *line;
-    
-    (void) line;
-    // char **args;
-    // int status;
+    char **args;
+    int status;
 
     while (1)
     {
-        line = readline("fstitou@fahd:~$ ");
-        // args = ft_split_line(line);
+        ft_putstr("$> ");
+        line = ft_read_line("");
+        args = ft_parse(line); //parsing args
+        status = ft_execute(args);// exec args , status holds the return value of the executed command
+        ft_free_2d_array(args); //add history or clear history or exit
+        free(line);
     }
     return (0);
 }
